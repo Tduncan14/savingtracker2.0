50b02531e3d8df2ea2917f412266d0bdec5bd923
tduncan14 <nbatre1@yahoo.com>
2026-04-28 20:23:56 -0500

    keep moving ofrward

---
+++ frontend/src/Components/Header/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Header.css'
+import Logo from '../../assets/images/logo-large.svg'
+import plus from '../../assets/images/icon-plus.svg'
+
+
+
+const Header = () => {
+
+    return (
+        <>
+            <div className="header">
+
+                <div className="logo">
+                    <img src={Logo} />
+
+                    <div><p>Savings Tracker </p></div>
+                </div>
+
+
+                <div>
+                    <button className="button"> <img src={plus} /> <p>New goal</p></button>
+                </div>
+
+            </div>
+
+
+        </>
+    )
+
+
+
+
+
+}
+
+
+
+export default Header

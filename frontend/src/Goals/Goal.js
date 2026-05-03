eb3c2b95f8bb6a5caf5c4ad96ae47ff936cd271c
tduncan14 <nbatre1@yahoo.com>
2026-05-02 21:50:39 -0500

    keep moving forward

--- frontend/src/Goals/Goal.js
+++ frontend/src/Goals/Goal.js
@@ -31,6 +31,97 @@ const Goals = () => {
 
             <div className="goalsBody">
 
+                <div className="One">
+                    <h1>MacBook Pro M4</h1>
+
+                    <div className="InputContainer">
+                        <h1>76%</h1>
+
+                        <progress value={75} max={100} />
+
+                    </div>
+
+
+                    <div>
+
+
+                        <div className="dateItem">
+                            <h3>Due 1  Jun 2026</h3>
+                        </div>
+                    </div>
+
+                </div>
+
+
+                <div className="Two">
+                    <h1>MacBook Pro M4</h1>
+
+                    <div className="InputContainer">
+                        <h1>76%</h1>
+
+                        <progress value={75} max={100} />
+
+                    </div>
+
+
+                    <div>
+
+
+                        <div className="dateItem">
+                            <h3>Due 1  Jun 2026</h3>
+                        </div>
+                    </div>
+
+                </div>
+
+
+                <div className="Three">
+                    <h1>MacBook Pro M4</h1>
+
+                    <div className="InputContainer">
+                        <h1>76%</h1>
+
+                        <progress value={75} max={100} />
+
+                    </div>
+
+
+                    <div>
+
+
+                        <div className="dateItem">
+                            <h3>Due 1  Jun 2026</h3>
+                        </div>
+                    </div>
+
+                </div>
+
+
+                <div className="Four">
+                    <h1>MacBook Pro M4</h1>
+
+                    <div className="InputContainer">
+                        <h1>76%</h1>
+
+                        <progress value={75} max={100} />
+
+                    </div>
+
+
+                    <div>
+
+
+                        <div className="dateItem">
+                            <h3>Due 1  Jun 2026</h3>
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+
+
             </div>
         </div>
     )
